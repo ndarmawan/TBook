044aaaf5bb765c4458619c4d23b755b21a0cb191
Nugha Darmawan <nugha.darmawan@gmail.com>
2013-12-11 08:13:45 +0700

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TBook::Application.config.secret_key_base = '0d7741e0f649fb90f31d316f6835168a394c044695949e8082e6da314c3d543dbcf431bd0ae5d792b2982a4016702a2093b1cf031fc271b74e9534c8400e9713'
